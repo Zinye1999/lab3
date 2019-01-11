e07b2e552c6295b7b349cddead2da5e52b167261
Zinye1999 <46591167+Zinye1999@users.noreply.github.com>
2019-01-11 16:14:48 +0700

    Add files via upload

---
+++ lab3_3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a=2,x=0;
+   
+    while(a<=69)
+    {
+        x=x+(1/a);
+        a++;
+        
+    }
+    cout<<x;
+    
+}
